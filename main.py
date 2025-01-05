0cc9bc27285dbf8e563a4e952801d77d0c50a6a5
Doug Zuniga <softdevninja@gmail.com>
2025-01-05 01:01:11 -0500

    Included program termination

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python3
+# *_* coding: utf-8 *_*
+
+"""
+Requesting IPv4 Address to get classification based on range. 
+
+References: 
+    https://www.meridianoutpost.com/resources/articles/IP-classes.php
+
+Example:
+    Class A -> Used for networks with large number of hosts
+    Class B -> Used for medium to large networks
+    Class C -> Used for small local area networks
+    Class D -> Used for multicasting
+    Class E -> Reserved for research
+"""
+__version__ = "1.0.0"
+__author__ = "Doug Zuniga"
+__email__ = "softdevninja@gmail.com"
+
 import re
+import sys
 
 
 def main() -> None:
@@ -7,17 +27,23 @@ def main() -> None:
     pattern: str = re.compile(r"\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}")
 
     IP_Address = input("Please enter IPv4 address for classification: ")
+    print(IP_Address)
 
     result = pattern.match(IP_Address)
 
     if is_valid_address(result):
         print("A match")
     else:
-        print("Entry not accepted")
+        exit_program()
 
 
 def is_valid_address(result: float) -> bool:
     return bool(result)
+
+
+def exit_program() -> None:
+    print(f"Entry not accepted. Terminating program, good bye!.")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
